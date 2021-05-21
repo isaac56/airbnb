409e40b88f7f7f361fbfe3524d6df64742b56288
okstring <milkoi7801@gmail.com>
2021-05-21 11:11:23 +0900

    [#5] feat: main 히어로 이미지 추가

--- iOS/airbnb/airbnb/Search/SearchViewController.swift
+++ iOS/airbnb/airbnb/Search/SearchViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
