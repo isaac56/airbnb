dc79b609d134743c70452691d4497303e8226a4f
정이삭 <isaac56@naver.com>
2021-05-29 04:21:04 +0900

    [#12] feat: Create 'request DTOs' to create a accommodation
    숙소 등록에 필요한 DTO 생성했습니다.

---
+++ BE/airbnb/src/main/java/team14/airbnb/domain/dto/request/accommodation/AccommodationCreateDto.java
@@ -0,0 +1,58 @@
+package team14.airbnb.domain.dto.request.accommodation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import team14.airbnb.domain.aggregate.accommodation.Accommodation;
+import team14.airbnb.domain.aggregate.user.User;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class AccommodationCreateDto {
+    @NotEmpty(message = "숙소 이름은 필수입니다.")
+    private String name;
+
+    @NotNull(message = "숙소 기본 가격은 필수입니다.")
+    private Integer basicFee;
+
+    private Integer weekendFee;
+
+    private Integer cleaningFee;
+
+    private String titleImageUrl;
+
+    @NotEmpty(message = "숙소 설명은 필수입니다.")
+    private String description;
+
+    @NotNull(message = "숙소 주소는 필수입니다.")
+    @Valid
+    private AccommodationAddressDto address;
+
+    @NotNull(message = "숙소 기본 정보는 필수입니다.")
+    @Valid
+    private DetailConditionDto detailCondition;
+
+    private List<String> options;
+
+    private List<String> imageUrls;
+
+    private List<String> hashTags;
+
+    public Accommodation toEntity(User host) {
+        return new Accommodation(
+                name,
+                basicFee,
+                weekendFee,
+                cleaningFee,
+                titleImageUrl,
+                description,
+                host,
+                detailCondition.toEntity(),
+                address.toEntity()
+        );
+    }
+}
