67e94e83b066c90eb9b7bf5b79087e3d89eb9d63
정이삭 <isaac56@naver.com>
2021-05-29 04:19:40 +0900

    feat: Create a GlobalExceptionHandler

---
+++ BE/airbnb/src/main/java/team14/airbnb/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package team14.airbnb;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import team14.airbnb.domain.dto.response.ApiResult;
+import team14.airbnb.exception.NotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResult validationException(MethodArgumentNotValidException exception) {
+        return ApiResult.fail(exception.getAllErrors().stream().
+                map(x -> x.getDefaultMessage()).
+                reduce((a, b) -> a + System.lineSeparator() + b).orElse("유효성 검사 실패"));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResult notFoundException(NotFoundException notFoundException) {
+        return ApiResult.fail(notFoundException);
+    }
+}
