c82eebc05bd1b92982964b87d7c4a217aa7df5e8
정이삭 <isaac56@naver.com>
2021-05-29 13:21:06 +0900

    [#6] feat: Change findByRegions to return only available accommodations
    findByRegions에서 해당 기간 동안 reservation이 없는 숙소만 반환하도록 수정

--- BE/airbnb/src/main/java/team14/airbnb/repository/AccommodationRepositoryCustom.java
+++ BE/airbnb/src/main/java/team14/airbnb/repository/AccommodationRepositoryCustom.java
@@ -2,9 +2,10 @@ package team14.airbnb.repository;
 
 import team14.airbnb.domain.aggregate.accommodation.Accommodation;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AccommodationRepositoryCustom {
 
-    List<Accommodation> findByRegionsCustom(String region1, String region2, String region3);
+    List<Accommodation> findByRegionsCustom(String region1, String region2, String region3, LocalDate startDate, LocalDate endDate);
 }
--- BE/airbnb/src/main/java/team14/airbnb/repository/AccommodationRepositoryCustomImpl.java
+++ BE/airbnb/src/main/java/team14/airbnb/repository/AccommodationRepositoryCustomImpl.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import team14.airbnb.domain.aggregate.accommodation.Accommodation;
 import team14.airbnb.domain.aggregate.accommodation.QAccommodation;
 import team14.airbnb.domain.aggregate.accommodation.QAccommodationAddress;
+import team14.airbnb.domain.aggregate.reservation.QReservation;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AccommodationRepositoryCustomImpl implements AccommodationRepositoryCustom {
@@ -17,24 +19,36 @@ public class AccommodationRepositoryCustomImpl implements AccommodationRepositor
         this.jpaQueryFactory = jpaQueryFactory;
     }
 
-    public List<Accommodation> findByRegionsCustom(String region1, String region2, String region3) {
+    public List<Accommodation> findByRegionsCustom(String region1, String region2, String region3, LocalDate startDate, LocalDate endDate) {
         QAccommodation accommodation = QAccommodation.accommodation;
         QAccommodationAddress accommodationAddress = QAccommodationAddress.accommodationAddress;
+        QReservation reservation = QReservation.reservation;
 
-        BooleanBuilder builder = new BooleanBuilder();
+        BooleanBuilder regionCondition = new BooleanBuilder();
         if (region1 != null && !region1.isEmpty()) {
-            builder.and(accommodationAddress.region1DepthName.eq(region1));
+            regionCondition.and(accommodationAddress.region1DepthName.eq(region1));
         }
         if (region2 != null && !region2.isEmpty()) {
-            builder.and(accommodationAddress.region2DepthName.eq(region2));
+            regionCondition.and(accommodationAddress.region2DepthName.eq(region2));
         }
         if (region3 != null && !region3.isEmpty()) {
-            builder.and(accommodationAddress.region3DepthName.eq(region3));
+            regionCondition.and(accommodationAddress.region3DepthName.eq(region3));
         }
 
+        BooleanBuilder reservationCondition = new BooleanBuilder();
+        reservationCondition.and(accommodation.id.eq(reservation.accommodation.id))
+                .and(
+                        reservation.startDate.between(startDate, endDate).and(reservation.startDate.ne(endDate))
+                                .or(reservation.endDate.between(startDate, endDate).and(reservation.endDate.ne(startDate)))
+                                .or(reservation.startDate.before(startDate).and(reservation.endDate.after(endDate)))
+                );
+
         return jpaQueryFactory.selectFrom(accommodation)
-                .innerJoin(accommodation.accommodationAddress, accommodationAddress)
-                .on(builder)
+                .innerJoin(accommodationAddress)
+                .on(regionCondition)
+                .leftJoin(reservation)
+                .on(reservationCondition)
+                .where(reservation.id.isNull())
                 .fetch();
     }
 }
