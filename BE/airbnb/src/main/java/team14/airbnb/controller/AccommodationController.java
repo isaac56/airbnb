8c8b43e452f60b0c661f20b3e538307f80efa6d3
정이삭 <isaac56@naver.com>
2021-05-30 13:52:34 +0900

    [#12] feat: Add a searchByLocation in controller

--- BE/airbnb/src/main/java/team14/airbnb/controller/AccommodationController.java
+++ BE/airbnb/src/main/java/team14/airbnb/controller/AccommodationController.java
@@ -3,7 +3,8 @@ package team14.airbnb.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import team14.airbnb.domain.aggregate.user.User;
-import team14.airbnb.domain.dto.request.accommodation.AccommodationCreateDto;
+import team14.airbnb.domain.dto.request.accommodation.CreateDto;
+import team14.airbnb.domain.dto.request.accommodation.SearchByLocationDto;
 import team14.airbnb.domain.dto.response.ApiResult;
 import team14.airbnb.exception.NotFoundException;
 import team14.airbnb.repository.UserRepository;
@@ -30,9 +31,16 @@ public class AccommodationController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ApiResult create(@RequestBody @Valid AccommodationCreateDto accommodationCreateDto) {
+    public ApiResult create(@RequestBody @Valid CreateDto createDto) {
         User user = getUser(); //로그인 구현 전까지 사용하기 위한 임시 코드
-        accommodationService.create(accommodationCreateDto, user);
+        accommodationService.create(createDto, user);
         return ApiResult.ok();
+    }
+
+    @GetMapping("/list")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResult searchByLocation(@Valid SearchByLocationDto searchByLocationDto) {
+        User user = getUser();
+        return ApiResult.succeed(accommodationService.searchByLocation(searchByLocationDto, user));
     }
 }
