2152e1d52ccc8245bedd4b6b711cfd7e8ee2d735
정이삭 <isaac56@naver.com>
2021-05-30 14:48:56 +0900

    [#1] feat: Add nickname,oauth_resource_server columns into user table

--- BE/airbnb/src/main/java/team14/airbnb/domain/aggregate/user/User.java
+++ BE/airbnb/src/main/java/team14/airbnb/domain/aggregate/user/User.java
@@ -25,13 +25,16 @@ public class User {
 
     private String email;
 
+    private String nickname;
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     @OrderBy("createdTime desc")
     private List<Wish> wishes = new ArrayList<>();
 
-    public User(String email) {
+    public User(String email, String nickname) {
         this.email = email;
+        this.nickname = nickname;
     }
 
     public void addWish(Accommodation accommodation) {
--- BE/airbnb/src/main/resources/data.sql
+++ BE/airbnb/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO `user` (`email`) VALUES ('temp@temp.com');
+INSERT INTO `user` (`email`, `nickname`) VALUES ('temp@temp.com', "이삭입니다");
