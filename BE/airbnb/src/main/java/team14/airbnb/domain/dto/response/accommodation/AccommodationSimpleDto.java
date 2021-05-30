ff3097399db2348bf0612111fda932d08ce9baba
정이삭 <isaac56@naver.com>
2021-05-30 13:12:33 +0900

    [#12] feat: Create a AccommodationSimpleDto
    1. 숙소 목록을 조회할 때, 숙소 하나에 해당하는 Response DTO 생성

---
+++ BE/airbnb/src/main/java/team14/airbnb/domain/dto/response/accommodation/AccommodationSimpleDto.java
@@ -0,0 +1,67 @@
+package team14.airbnb.domain.dto.response.accommodation;
+
+import lombok.Builder;
+import lombok.Getter;
+import team14.airbnb.domain.aggregate.accommodation.Accommodation;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class AccommodationSimpleDto {
+    private long id;
+
+    private String name;
+
+    private int totalFee;
+
+    private int dailyFee;
+
+    private String addressName;
+
+    private String roadAddressName;
+
+    private Double x;
+
+    private Double y;
+
+    private Integer maxOfPeople;
+
+    private String type;
+
+    private Integer numberOfRoom;
+
+    private Integer numberOfToilet;
+
+    private List<String> options;
+
+    private String titleImage;
+
+    private boolean wished;
+
+    private AccommodationSimpleDto() {
+    }
+
+    public static AccommodationSimpleDto of(Accommodation accommodation, Set<Integer> wishSet) {
+
+        return builder()
+                .id(accommodation.getId())
+                .name(accommodation.getName())
+                .totalFee(accommodation.getTotalFee())
+                .dailyFee(accommodation.getDailyFee())
+                .addressName(accommodation.getAddressName())
+                .roadAddressName(accommodation.getRoadAddressName())
+                .x(accommodation.getX())
+                .y(accommodation.getY())
+                .type(accommodation.getTypeName())
+                .maxOfPeople(accommodation.getMaxOfPerson())
+                .numberOfRoom(accommodation.getMaxOfPerson())
+                .numberOfToilet(accommodation.getNumberOfToilet())
+                .options(accommodation.getAccommodationOptions().stream().map(x -> x.getName()).collect(Collectors.toList()))
+                .titleImage(accommodation.getTitleImageUrl())
+                .wished(wishSet.contains(accommodation.getId()))
+                .build();
+    }
+}
